f133fc552edd3d4a1a03820ede8ed73919b72b3e
Jordan Silke <51223540+jsilke@users.noreply.github.com>
2022-05-20 19:26:05 -0400

    Implement time travel to allow the user to traverse the game state.

--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,17 @@ type Squares = {
 
 interface SquareProps {
   value: Value;
-  onClick: any;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 interface BoardProps extends Squares {
-  onClick: any;
+  onClick: any; // The current architecture causes an error if this type is made more specific.
 }
 
 interface GameState {
   history: Array<Squares>;
   xIsNext: boolean;
+  stepNumber: number;
 }
 
 const Square = (props: SquareProps) => {
@@ -70,14 +71,16 @@ class Game extends React.Component<any, GameState> {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i: number) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -86,14 +89,33 @@ class Game extends React.Component<any, GameState> {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        `Go to move # ${move}` :
+        'Go to game start.';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -112,7 +134,7 @@ class Game extends React.Component<any, GameState> {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -138,6 +160,8 @@ const calculateWinner = (squares: Array<'X' | 'O' | null>) => {
   }
   return null;
 };
+
+// TODO - Add logic for drawing.
 
 // ========================================
 
